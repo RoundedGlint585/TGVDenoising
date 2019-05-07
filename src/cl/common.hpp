cbf1416fdd324d58bf1f66e8bc32ffc67d309276
Smolyakov Daniil <danonofficial@gmail.com>
2019-05-07 15:32:03 +0300

    Added missing headers

---
+++ src/cl/common.hpp
@@ -0,0 +1,20 @@
+//
+// Created by roundedglint585 on 5/7/19.
+//
+
+#ifndef TGV_COMMON_HPP
+#define TGV_COMMON_HPP
+#include "epsilon.h"
+#include "gradient.h"
+#include "copy.h"
+#include "mulMatrixOnConstant.h"
+#include "project.h"
+#include "sumOfMatrix.h"
+#include "transpondedGradient.h"
+#include "transpondedEpsilon.h"
+#include "calculateHist.h"
+#include "prox.h"
+#include "clear.h"
+#include "anorm.h"
+#include "sqrt.h"
+#endif //TGV_COMMON_HPP
