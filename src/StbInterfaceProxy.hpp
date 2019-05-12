65068e962b74f796dfe51bf99cf1f7b1686c9f19
Smolyakov Daniil <danonofficial@gmail.com>
2019-05-12 10:56:15 +0300

    ADded missed header

---
+++ src/StbInterfaceProxy.hpp
@@ -0,0 +1,9 @@
+//
+// Created by roundedglint585 on 5/11/19.
+//
+
+#ifndef TGV_STBINTERFACEPROXY_HPP
+#define TGV_STBINTERFACEPROXY_HPP
+#include <stb_image.h>
+#include <stb_image_write.h>
+#endif //TGV_STBINTERFACEPROXY_HPP
